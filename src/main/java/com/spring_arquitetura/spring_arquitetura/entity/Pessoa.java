8142a457b0ad8213e208b264e3abaddf3b9fc08f
João Victor da Silva <joaovictor00788123@gmail.com>
2020-05-29 20:31:17 -0300

    Version 1.1

--- src/main/java/com/spring_arquitetura/spring_arquitetura/entity/Pessoa.java
+++ src/main/java/com/spring_arquitetura/spring_arquitetura/entity/Pessoa.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,13 +11,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "tabela_pessoa")
 @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
@@ -39,5 +31,37 @@ public class Pessoa implements Serializable{
     private int idade;
     private double altura;
     private String sexo;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+    
      
 }
